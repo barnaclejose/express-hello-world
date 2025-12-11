aa2a80a872a1f597fe8075a5dde7d09328a1165c
barnaclejose <septa.me@proton.me>
2025-12-11 01:35:04 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -21,8 +21,17 @@ app.get("/player_api.php", function(req, res){
 	var m_var_Server = req.query.server;
 	var m_var_Username = req.query.username;
 	var m_var_Password = req.query.password;
+	var m_var_Action = req.query.action; //get_profile get_server_info get_live_streams get_channel_categories
 	
-	res.type('json').send("[{'a': 1}, {'a': 2}]");
+	
+	if(m_var_Action == "get_profile"){
+		var returnVal = {"user_info":{"username":"020a99bbf5","password":"aaa38a3ab0","message":"Welcome to World 8K","auth":1,"status":"Active","exp_date":"1768031057","is_trial":"0","active_cons":"1","created_at":"1765352657","max_connections":"1","allowed_output_formats":["m3u8","ts","rtmp"]},"server_info":{"url":"cf.business-cdn.me","port":"80","https_port":"443","server_protocol":"http","rtmp_port":"25462","timezone":"Europe\/Amsterdam","timestamp_now":1765445611,"time_now":"2025-12-11 10:33:31","process":true}}
+		res.type('json').send(returnVal);
+	}else{
+		res.type('json').send("[{'a': 1}, {'a': 2}]");
+
+	}
+	
 	
 });
 
